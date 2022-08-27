de89a3ddaeded2b0b636a77a31e7f5ed260b1e61
DAN <dan.nemer@gmail.com>
2022-08-27 16:46:31 -0400

    if statement start

--- script.js
+++ script.js
@@ -4,20 +4,53 @@ var generateBtn = document.querySelector("#generate");
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var abcUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var abcLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var chararacter = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
-
-function determineLength() {
-  passwordLength = prompt("Choose the length you would like your password to be: ");
-}
-
-
+var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
 var confirmAbcLow;
 var confirmAbcUp;
 var confirmNumber;
 var confirmChar;
 
-funtion 
+function determineLength() {
+  passwordLength = prompt("Choose the length you would like your password to be: ");
+}
+
+  if (passwordLength<8) {
+  alert("Passwork length must be 8-128 characters long");
+  determineLength();
+  }
+  else if (passwordLength>128){
+    alert("Passwork length must be 8-128 characters long")
+  }
+
+  if (passwordLength<8) {
+    alert("Passwork length must be 8-128 characters long");
+    determineLength();
+    }
+    else if (passwordLength>128){
+      alert("Passwork length must be 8-128 characters long")
+    }
+
+    if (passwordLength<8) {
+      alert("Passwork length must be 8-128 characters long");
+      determineLength();
+      }
+      else if (passwordLength>128){
+        alert("Passwork length must be 8-128 characters long")
+      }
+
+      if (passwordLength<8) {
+        alert("Passwork length must be 8-128 characters long");
+        determineLength();
+        }
+        else if (passwordLength>128){
+          alert("Passwork length must be 8-128 characters long")
+        }
+
+
+
+
+
 
 
 // Write password to the #password input
