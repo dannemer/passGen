b3605d3df4cbcf72535bfda60c6c9484d39a179e
DAN <dan.nemer@gmail.com>
2022-08-28 22:59:02 -0400

    too many functions

--- script.js
+++ script.js
@@ -1,55 +1,57 @@
 // Assignment code here
-var generateBtn = document.querySelector("#generate");
+window.onload = alert("Please click the Generate Password button to begin")
 
-window.onload = alert("Please click Generate asswork to begin.");
+var generateBtn = document.querySelector("#generate").addEventListener("click", writePassword);
 
 var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 var abcUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var abcLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-var confirmAbcLow = window.prompt ("Would you like your password to contain lower casee letter?);
-var confirmAbcUp; window.prompt ("Would you like your password to contain lower casee letter?);
-var confirmNumber; window.prompt ("Would you like your password to contain lower casee letter?);
-var confirmChar; window.prompt ("Would you like your password to contain lower casee letter?);
-
+var confirmAbcLow;
+var confirmAbcUp; 
+var confirmNumber;
+var confirmChar;
+var confirmLength;
 var passwordLength;
 
+/////////////////////////////////////////////
+
 function determineLength() {
-  passwordLength = window.prompt("Choose the numer of characters you would like your password to be (between 8-128): ");
+  var passwordLength = window.prompt("Choose the number of characters you would like your password to be (between 8-128): ");
 
   if (passwordLength<8) {
-  alert("Passwork length must be 8-128 characters long");
-  determineLength();
+  alert("Password length must be 8-128 characters long");
   }
   else if (passwordLength>128){
-    alert("Passwork length must be 8-128 characters long")
-  }
+  alert("Passwork length must be 8-128 characters long")
+  } else {
   return passwordLength;
 }
 
-  function confirmAbcUp() {
-
+function confirmAbcLow() {
+  var abcUp = window.prompt("Would like you password to contain uppercase letter?");
   }
 
-  function confirmChar() {
-
+function confirmAbcUp() {
+  var abcLow = window.prompt("Would like you password to contain lowercase letters?");
   }
 
-  function confirmNumber () {
-
+function confirmChar() {
+  var character = window.prompt("Would you like your password to contain special characters?");
   }
 
+function confirmNumber () {
+  var number = window.prompt("Would you like your passwrod to contain numbers?")
+  } 
 
+function generatePassword () {
+  var result = ""
+  result = generatePassword();
+  return result;
+}
 
-
-  function 
-
-
-
-
-
-
+/////////////////////////////////////////////////////
 
 // Write password to the #password input
 function writePassword() {
@@ -58,22 +60,9 @@ function writePassword() {
 
   passwordText.value = password;
 
-}
+  var reset = window.alert("Would you like a different password?")
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-funtion generatePassword () {
-
-
+  If(reset) {
+      writePassword();
+  }
 }
