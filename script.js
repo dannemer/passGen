4c544924d439ee32f5f6aff2ea9bb9e0a597f83d
DAN <dan.nemer@gmail.com>
2022-08-30 12:09:42 -0400

    cleaned up

--- script.js
+++ script.js
@@ -1,76 +1,51 @@
-// Assignment code here
-window.onload = alert("Please click the Generate Password button to begin")
+var generateBtn = document.querySelector("#generate");
 
-var generateBtn = document.querySelector("#generate").addEventListener("click", writePassword);
-
-function determineLength() {
-  var passwordLength = window.prompt("Choose the number of characters you would like your password to be (between 8-128): ");
-
-  if (passwordLength<8) {
-  alert("Password length must be 8-128 characters long");
-  }
-  else if (passwordLength>128){
-  alert("Passwork length must be 8-128 characters long")
-  } else {
-  return passwordLength;
-  }
-}
+var generateBtn.addEventListener("click", writePassword);
 
 function generatePassword () {
-
-  var confirmlength = prompt("Choose the number of characters you would like your password to be (between 8-128);
-  
-  var length = parseInt(conformLength)
-
-  if (isNaN(passwordLength)) {
-    window.alert("Please choose a total number between 8-128")
-    return
-  }
-
-  if (passwordLength < 8 || passwordLength > 128) {
-    window.alert("Please choose a total number between 8-128")
-    return
-  }
-
-  var confirmAbcUp = confirm("Would like you password to contain uppercase letter?");
-  var confirmCharacter = confirm("Would you like your password to contain special characters?");
-  var confirmAbcLow = confirm("Would like you password to contain lowercase letters?");
-  var confirmNumbers = confirm("Would you like your passwrod to contain numbers?")
-
   var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   var abcUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var abcLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var character = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
+  var password = [];
+  
+  var confirmLength = prompt("Choose the number of characters you would like your password to be (between 8-128.");
+  
+    if (confirmLength < 8 || confirmLength > 128 || isNaN(confirmLength)) {
+      alert ("Choice must be between 8 - 128 characters");
+      return;
+    }
 
-  var password = []
+  var confirmAbcUp = confirm("Would like you password to contain uppercase letter?");
+    if (confirmAbcUp === true) {
+      for (var i = 0; i < abcUp.length; i++) {
+      password.push(abcUp[i])
+      }
+    }
 
-  for (var i = 0; i < abcLow; i++) {
+    var confirmAbcLow = confirm("Would like you password to contain lowercase letter?");
+    if (confirmAbcLow === true) {
+      for (var i = 0; i < abcLow.length; i++) {
+      password.push(abcLow[i])
+      }
+    }
 
-  }
+    var confirmNumbers = confirm("Would like you password to contain numbers");
+    if (confirmNumbers === true) {
+      for (var i = 0; i < numbers.length; i++) {
+      password.push(numbers[i])
+      }
+    }
 
-  if (confirmAbcLow === true) {
-    password.push(abcLow)
-  }
-
-  if (confirmAbcUp === true) {
-    password.push(abcUp)
-  }
-
-  if (confirmNumbers === true) {
-    password.push(numbers)
-  }
-
-  if (confirmCharacter === true) {
-    password.push(character)
-  }
-
-console.log(password)
-
+    var confirmChar = confirm("Would like you password to contain special characters");
+    if (confirmChar === true) {
+      for (var i = 0; i < character.length; i++) {
+      password.push(character[i])
+      }
+    }
 
 
-  result = generatePassword();
-  return password;
-}
+var generatePassword = "";
 
 
 
